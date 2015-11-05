fff90cf8416170cdf9154fab70ef58bc71362922
French Ben <me+git@frenchben.com>
2015-11-04 17:40:11 -0800

    Added pull count to images - Fixes 1175
    Signed-off-by: French Ben <me+git@frenchben.com>

--- src/components/ImageCard.react.js
+++ src/components/ImageCard.react.js
@@ -8,6 +8,7 @@ import containerActions from '../actions/ContainerActions';
 import containerStore from '../stores/ContainerStore';
 import tagStore from '../stores/TagStore';
 import tagActions from '../actions/TagActions';
+import numeral from 'numeral';
 
 var ImageCard = React.createClass({
   mixins: [Router.Navigation],
@@ -147,6 +148,7 @@ var ImageCard = React.createClass({
         <span className="icon icon-badge-private"></span>
       );
     }
+    let pullCount = (this.props.image.pull_count < 1000) ? numeral(this.props.image.pull_count) : numeral(this.props.image.pull_count).format('0.0 a').toUpperCase();
     return (
       <div className="image-item">
         <div className="overlay menu-overlay">
@@ -186,6 +188,8 @@ var ImageCard = React.createClass({
             <div className="favorites">
               <span className="icon icon-favorite"></span>
               <span className="text">{this.props.image.star_count}</span>
+              <span className="icon icon-download"></span>
+              <span className="text">{pullCount}</span>
             </div>
             <div className="more-menu" onClick={self.handleMenuOverlayClick}>
               <span className="icon icon-more"></span>
